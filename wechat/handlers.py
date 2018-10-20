935ab5c596a07437f4399b0d846abc0fe23c8ec4
ElvaWu <39610243+ElvaWu@users.noreply.github.com>
2018-10-20 14:15:13 +0800

    Update handlers.py

--- wechat/handlers.py
+++ wechat/handlers.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 from wechat.wrapper import WeChatHandler
-
-
+from wechat.models import Activity,Ticket,User
+from WeChatTicket import settings
 __author__ = "Epsirom"
 
 
@@ -65,3 +65,46 @@ class BookEmptyHandler(WeChatHandler):
 
     def handle(self):
         return self.reply_text(self.get_message('book_empty'))
+
+class BookWhatHandler(WeChatHandler):
+    def check(self):
+        return self.is_text('抢啥') or self.is_event_click(self.view.event_keys['book_what'])
+
+    def handle(self):
+        details = []
+        activities = Activity.objects.filter(status=Activity.STATUS_PUBLISHED)
+        if activities:
+            for item in activities:
+                details.append({
+                    'Title':item.name,
+                    'Description':item.description,
+                    'PicUrl':item.pic_url,
+                    'Url':settings.get_url("/u/activity",{"id":item.id})
+                })
+
+            return self.reply_news(details)
+
+        else :
+            return self.reply_text(self.get_message('bind_account'))
+
+class FindOutTicketHandler(WeChatHandler):
+    def check(self):
+        return self.is_text('查票') or self.is_event_click(self.view.event_keys['get_ticket'])
+
+    def handle(self):
+        details = []
+        if not self.user.student_id:
+            return self.reply_text("您还未绑定")
+        tickets = Ticket.objects.filter(student_id=self.user.student_id,status=Ticket.STATUS_VALID)
+        if not tickets:
+            return self.reply_text("没有票可用")
+        else :
+            for item in tickets:
+                activity = Activity.objects.filter(key=self.input['Content'][3:],status=Activity.STATUS_PUBLISHED)
+                details.append({
+                    'Title': activity.name,
+                    'Description': activity.description,
+                    'PicUrl': activity.pic_url,
+                    'Url': settings.get_url("/u/ticket", {'openid':user.open_id,'ticket':item.unique_id})
+                })
+return self.reply_news(details)
